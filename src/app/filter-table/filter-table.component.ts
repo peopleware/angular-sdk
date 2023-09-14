c418c2b9351a38a11d3cd306d6e718eb18d75e4e
Mark Cuypers <mark_cuypers@peopleware.be>
2023-09-14 13:07:13 +0200

    Update the table-filter demo

--- src/app/filter-table/filter-table.component.ts
+++ src/app/filter-table/filter-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, LOCALE_ID, OnInit, signal, WritableSignal } from '@angular/core'
+import { Component, Inject, LOCALE_ID, OnInit, signal, TemplateRef, ViewChild, WritableSignal } from '@angular/core'
 import {
     Column,
     DateColumn,
@@ -7,14 +7,16 @@ import {
     SearchFilterComponent,
     TableComponent,
     TableRecord,
+    TemplateColumn,
     TextColumn
 } from '@ppwcode/ng-common-components'
 import { DateTime } from 'luxon'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { CommonModule, formatCurrency, formatPercent, getCurrencySymbol, NgIf } from '@angular/common'
 import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
 
 export interface Player extends Record<string, unknown> {
     id: number
@@ -34,7 +36,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1988, month: 7, day: 23 }),
         age: 35,
         income: 35000,
-        bonus: 10
+        bonus: 10,
+        active: true
     },
     {
         id: 2,
@@ -43,7 +46,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1989, month: 4, day: 2 }),
         age: 34,
         income: 37000,
-        bonus: 2
+        bonus: 2,
+        active: true
     },
     {
         id: 3,
@@ -52,7 +56,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1992, month: 10, day: 18 }),
         age: 31,
         income: 26000,
-        bonus: 5
+        bonus: 5,
+        active: true
     },
     {
         id: 4,
@@ -61,7 +66,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1991, month: 6, day: 28 }),
         age: 32,
         income: 23000,
-        bonus: 40
+        bonus: 40,
+        active: false
     },
     {
         id: 5,
@@ -70,7 +76,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1975, month: 3, day: 22 }),
         age: 48,
         income: 23000,
-        bonus: 30
+        bonus: 30,
+        active: false
     },
     {
         id: 6,
@@ -79,7 +86,8 @@ const PLAYERS_DATA: Array<Player> = [
         birthDate: DateTime.fromObject({ year: 1993, month: 7, day: 1 }),
         age: 30,
         income: 24000,
-        bonus: 20
+        bonus: 20,
+        active: true
     }
 ]
 
@@ -103,6 +111,7 @@ type SearchPlayersForm = {
         TableComponent,
         SearchFilterComponent,
         MatCardModule,
+        MatIconModule,
         MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule,
@@ -110,6 +119,7 @@ type SearchPlayersForm = {
     ]
 })
 export class FilterTableComponent implements OnInit {
+    @ViewChild('playerStatusTemplate', { static: true }) public playerStatusTemplate!: TemplateRef<any>
     public searchForm!: FormGroup
     public columns: Array<Column<Player, unknown>> = [
         new TextColumn('firstName', 'First name', 'firstName'),
@@ -119,7 +129,10 @@ export class FilterTableComponent implements OnInit {
         new NumberColumn('income', 'Income', 'income', (income: number) =>
             formatCurrency(income, this.locale, getCurrencySymbol('EUR', 'wide'), 'EUR', '3.2-2')
         ),
-        new NumberColumn('bonus', 'Bonus', 'bonus', (bonus: number) => formatPercent(bonus / 100, this.locale, '1.2-2'))
+        new NumberColumn('bonus', 'Bonus', 'bonus', (bonus: number) =>
+            formatPercent(bonus / 100, this.locale, '1.2-2')
+        ),
+        new TemplateColumn('active', 'Active', (player: Player) => this.playerStatusTemplate)
     ]
     public data = PLAYERS_DATA
     public selectedPlayersSignal: WritableSignal<Player[]> = signal([])
@@ -128,7 +141,7 @@ export class FilterTableComponent implements OnInit {
 
     public ngOnInit(): void {
         this.searchForm = new FormGroup<SearchPlayersForm>({
-            firstName: new FormControl('', { nonNullable: true }),
+            firstName: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
             lastName: new FormControl('', { nonNullable: true })
         })
     }
