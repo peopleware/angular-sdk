6e267a2f84610bf53a932ced0964ce4f595fd9e7
Mark Cuypers <mark_cuypers@peopleware.be>
2023-09-15 09:16:09 +0200

    Add paged-entities.mock

---
+++ projects/ppwcode/ng-async/src/lib/models/paged-entities.mock.ts
@@ -0,0 +1,25 @@
+import { PagedEntities, PagedEntitiesDto } from './paged-entities'
+
+export const createPagedEntitiesDtoMock = <TDto>(dtoEntities: Array<TDto>): PagedEntitiesDto<TDto> => {
+    return {
+        pageIndex: 1,
+        pageSize: dtoEntities.length ?? 20,
+        totalCount: dtoEntities.length,
+        totalPages: 1,
+        hasPreviousPage: false,
+        hasNextPage: false,
+        items: dtoEntities
+    }
+}
+
+export const createPagedEntitiesMock = <TModel>(models: Array<TModel>): PagedEntities<TModel> => {
+    return {
+        pageIndex: 1,
+        pageSize: models.length ?? 20,
+        totalCount: models.length,
+        totalPages: 1,
+        hasPreviousPage: false,
+        hasNextPage: false,
+        items: models
+    }
+}
