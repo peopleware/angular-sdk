5afc3f89d0a1b99e308b4d0320f100f80cbdf9e8
Glen Staes <glen_staes@peopleware.be>
2023-09-18 13:16:53 +0200

    Extend comments on PpwTableOptions

--- projects/ppwcode/ng-common-components/src/lib/table/options/table-options.ts
+++ projects/ppwcode/ng-common-components/src/lib/table/options/table-options.ts
@@ -1,11 +1,11 @@
 /**
- * Interface describing the options to
+ * Interface describing the options for a ppwcode table.
  */
 export interface PpwTableOptions<TRecord> {
     /**
      * The widths of the columns in the table.
      * The keys are the column names. Any string is allowed because column names are not limited to the keys of a record,
-     *
+     * because we can have columns that are calculated from other columns.
      * The values are the widths in CSS units: px, %, em, rem, ...
      */
     columnWidths?: Record<keyof Partial<TRecord> | string, string>
