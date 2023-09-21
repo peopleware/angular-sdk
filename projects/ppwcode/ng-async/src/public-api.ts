2b366a024c91cd9a2e916773631af2f67945a036
Mark Cuypers <mark_cuypers@peopleware.be>
2023-09-21 14:01:01 +0200

    Export the save-downloaded-file function

--- projects/ppwcode/ng-async/src/public-api.ts
+++ projects/ppwcode/ng-async/src/public-api.ts
@@ -5,6 +5,7 @@
 export * from './lib/async-result/async-result.component'
 export * from './lib/rxjs-operators/expect-http-error'
 export * from './lib/constants/error-codes'
+export * from './lib/downloads/save-downloaded-file'
 export * from './lib/models/async-result'
 export * from './lib/models/paged-entities'
 export * from './lib/models/paged-entities.mock'
