98ceebb2fefac8c0fa22f829bccf8c5c81ef4e53
Glen Staes <glen_staes@peopleware.be>
2023-10-06 15:15:04 +0200

    Add AbstractSearchFilterState to easily to store search filters

---
+++ projects/ppwcode/ng-state-management/src/lib/abstract-search-filter-state.ts
@@ -0,0 +1,31 @@
+import { inject } from '@angular/core'
+import { SESSION_STORAGE_TOKEN } from '@ppwcode/ng-common'
+import { SignalStore } from './signal-store/signal-store'
+
+export abstract class AbstractSearchFilterState<
+    TStateModel extends Record<string, unknown>
+> extends SignalStore<TStateModel> {
+    private sessionStorage: Storage | null = inject(SESSION_STORAGE_TOKEN, { optional: true })
+
+    abstract readonly sessionStorageKey: string
+    abstract getDefaultValues(): TStateModel
+
+    protected constructor() {
+        super()
+
+        this.initialize(this.getDefaultValues())
+        this.loadInitialState()
+    }
+
+    public saveSearchFilters<TKey extends keyof TStateModel>(key: TKey, value: TStateModel[TKey]): void {
+        this.patch({ [key]: value } as unknown as Partial<TStateModel>)
+        this.sessionStorage?.setItem(this.sessionStorageKey, JSON.stringify(this.snapshot))
+    }
+
+    private loadInitialState(): void {
+        const sessionStorageValue = this.sessionStorage?.getItem(this.sessionStorageKey)
+        if (sessionStorageValue) {
+            this.patch(JSON.parse(sessionStorageValue))
+        }
+    }
+}
