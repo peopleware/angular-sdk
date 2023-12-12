6a38247d33cdfcd059dec5aee67d2f3bf31b92e0
Mark Cuypers <mark_cuypers@peopleware.be>
2023-12-12 16:37:55 +0100

    Fix issue with filename retrieval from content disposition

---
+++ projects/ppwcode/ng-async/src/lib/models/file-download.spec.ts
@@ -0,0 +1,10 @@
+import { getFileNameFromContentDisposition } from './file-download'
+
+describe('getFileName', () => {
+    it('should get the filename from a content-disposition string', () => {
+        const contentDisposition =
+            'attachment; filename="Incentive EUR - Devloo BELGIUM NV - 20231212_1524.xlsx"; filename*=UTF-8\'\'Incentive%20EUR%20-%20Devloo%20BELGIUM%20NV%20-%2020231212_1524.xlsx'
+        const expected = 'Incentive EUR - Devloo BELGIUM NV - 20231212_1524.xlsx'
+        expect(getFileNameFromContentDisposition(contentDisposition)).toEqual(expected)
+    })
+})
--- projects/ppwcode/ng-async/src/lib/models/file-download.ts
+++ projects/ppwcode/ng-async/src/lib/models/file-download.ts
@@ -8,11 +8,19 @@ export interface FileDownload {
 
 export const httpResponseToFileDownload = (response: HttpResponse<Blob>): FileDownload => {
     const contentDisposition = response.headers.get('Content-Disposition')
+    const fileName = getFileNameFromContentDisposition(contentDisposition)
     return {
         blob: notUndefined(response.body || undefined),
-        fileName:
-            contentDisposition && contentDisposition.length && contentDisposition.indexOf('filename=') > -1
-                ? contentDisposition.split('filename=')[1].trim().replace(/['"]/g, '')
-                : undefined
+        fileName
     }
+}
+
+export const getFileNameFromContentDisposition = (contentDisposition?: string | null): string | undefined => {
+    if (!(contentDisposition && contentDisposition.length && contentDisposition.indexOf('filename=') > -1)) {
+        return undefined
+    }
+    const filenameStart = contentDisposition.split('filename=')[1]
+    const filename =
+        filenameStart?.length && filenameStart.indexOf(';') > -1 ? filenameStart.split(';')[0] : filenameStart
+    return filename?.length ? filename.trim().replace(/['"]/g, '') : undefined
 }
