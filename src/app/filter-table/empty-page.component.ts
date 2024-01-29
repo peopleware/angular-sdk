3afaf10cd2f9a1b43b9b4e8fb840f485208c4d4b
Glen Staes <glen_staes@peopleware.be>
2024-01-29 14:58:24 +0100

    Add demo showcasing an empty table page

---
+++ src/app/filter-table/empty-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'ppw-empty-table-page',
+    template: '<p>Page is empty</p>',
+    standalone: true
+})
+export class EmptyTablePageComponent {}
