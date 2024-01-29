3b2eba32bf0e8665591a69096d0638be97f92f31
Glen Staes <glen_staes@peopleware.be>
2024-01-29 15:46:05 +0100

    Use empty async result templates in table demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { LOCALE_ID, NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgOptimizedImage, registerLocaleData } from '@angular/common'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import localeEn from '@angular/common/locales/en-BE'
+import { LOCALE_ID, NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, MatDateFormats } from '@angular/material/core'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { TitleStrategy } from '@angular/router'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { PPW_ASYNC_RESULT_DEFAULT_OPTIONS, PpwAsyncResultDefaultOptions } from '@ppwcode/ng-async'
 import { PPW_TABLE_DEFAULT_OPTIONS } from '@ppwcode/ng-common-components'
+import { TranslatedPageTitleStrategy } from '@ppwcode/ng-router'
+import { WireframeComponent } from '@ppwcode/ng-wireframe'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { WireframeComponent } from '@ppwcode/ng-wireframe'
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-import { HttpClient, HttpClientModule } from '@angular/common/http'
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, MatDateFormats } from '@angular/material/core'
-import { NgOptimizedImage, registerLocaleData } from '@angular/common'
-import { MatIconModule } from '@angular/material/icon'
-import { TitleStrategy } from '@angular/router'
-import { TranslatedPageTitleStrategy } from '@ppwcode/ng-router'
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { FormsModule } from '@angular/forms'
+import { EmptyAsyncResultComponent } from './filter-table/empty-async-result.component'
 import { getLuxonFormatter } from './filter-table/filter-table.component'
 
 registerLocaleData(localeEn)
@@ -63,7 +65,11 @@ const ppwcodeComponents = [WireframeComponent]
         { provide: LOCALE_ID, useValue: 'en-US' },
         { provide: TitleStrategy, useClass: TranslatedPageTitleStrategy },
         { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS },
-        { provide: PPW_TABLE_DEFAULT_OPTIONS, useValue: { dateColumnFormatter: getLuxonFormatter('dd/MM/yyyy') } }
+        { provide: PPW_TABLE_DEFAULT_OPTIONS, useValue: { dateColumnFormatter: getLuxonFormatter('dd/MM/yyyy') } },
+        {
+            provide: PPW_ASYNC_RESULT_DEFAULT_OPTIONS,
+            useValue: { emptyResultComponent: EmptyAsyncResultComponent } as PpwAsyncResultDefaultOptions
+        }
     ],
     bootstrap: [AppComponent]
 })
---
+++ src/app/filter-table/empty-async-result.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { MessageBarComponent, Severity } from '@ppwcode/ng-common-components'
+
+@Component({
+    selector: 'ppw-empty-async-result-component',
+    template:
+        '<ppw-message-bar [severity]="Severity.warning" message="This async result returned unexpectedly with a null value."></ppw-message-bar>',
+    imports: [MessageBarComponent],
+    standalone: true
+})
+export class EmptyAsyncResultComponent {
+    protected readonly Severity = Severity
+}
