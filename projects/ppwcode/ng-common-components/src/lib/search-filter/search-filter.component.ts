74b33f0ec4c76c10f342c7a22b2f2afffdaa5f29
Mark Cuypers <mark_cuypers@peopleware.be>
2024-03-08 09:49:48 +0100

    Update the filters to allow passing a custom button labels and hide the reset button when not desired

--- projects/ppwcode/ng-common-components/src/lib/search-filter/search-filter.component.ts
+++ projects/ppwcode/ng-common-components/src/lib/search-filter/search-filter.component.ts
@@ -2,16 +2,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
+import { CommonModule } from '@angular/common'
 
 @Component({
     selector: 'ppw-search-filter',
     templateUrl: './search-filter.component.html',
     styleUrls: ['./search-filter.component.scss'],
-    imports: [MatCardModule, MatButtonModule],
+    imports: [MatCardModule, MatButtonModule, CommonModule],
     standalone: true
 })
 export class SearchFilterComponent {
     @Input() public submitDisabled = false
+    @Input() public allowReset = true
+    @Input() public searchLabel: string = 'Search'
+    @Input() public resetLabel: string = 'Reset'
     @Output() public search: EventEmitter<void> = new EventEmitter<void>()
     @Output() public reset: EventEmitter<void> = new EventEmitter<void>()
 }
