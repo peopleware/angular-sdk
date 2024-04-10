259987cf86aa57b5e173223c3dc3d471d481331e
Glen Staes <glen_staes@peopleware.be>
2024-04-10 09:00:37 +0200

    Fix return type of cleanupRequestData

--- projects/ppwcode/ng-async/src/lib/api-calls/request-data.ts
+++ projects/ppwcode/ng-async/src/lib/api-calls/request-data.ts
@@ -1,10 +1,12 @@
+declare type RequestDataValueType = string | number | boolean | readonly (string | number | boolean)[]
+
 export const cleanupRequestData = (data: { [key: string]: unknown }) => {
     const dataKeys: Array<string> = Object.keys(data)
-    const cleanedData: { [key: string]: unknown } = {}
+    const cleanedData: { [param: string]: RequestDataValueType } = {}
 
     dataKeys.forEach((dataKey) => {
         if (data[dataKey] !== undefined && data[dataKey] !== null) {
-            cleanedData[dataKey] = data[dataKey]
+            cleanedData[dataKey] = data[dataKey] as RequestDataValueType
         }
     })
 
