78c341bcec43410c806a16b4270af483fbd886ca
Glen Staes <glen_staes@peopleware.be>
2024-04-23 11:25:31 +0200

    chore(common): extend api translate pipe tests

--- projects/ppwcode/ng-common/src/lib/pipes/api-translate.pipe.spec.ts
+++ projects/ppwcode/ng-common/src/lib/pipes/api-translate.pipe.spec.ts
@@ -1,30 +1,55 @@
-import { PpwTranslationMap } from '../models/ppw-translation-map.model'
-import { PpwApiTranslatePipe } from '@ppwcode/ng-common'
+import { LOCALE_ID } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
+import { PpwApiTranslatePipe } from '@ppwcode/ng-common'
+import { PpwTranslationMap } from '../models/ppw-translation-map.model'
 
 const mockTranslationMap: PpwTranslationMap = {
-    nl: 'testNl',
-    en: 'testEn',
-    fr: 'testFr'
+    nl: 'testnl',
+    en: 'testen',
+    fr: 'testfr'
 }
 
 describe('Api translate pipe should work properly', () => {
     let apiTranslatePipe: PpwApiTranslatePipe
+    const languages = ['nl', 'en', 'fr']
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [PpwApiTranslatePipe]
+    languages.forEach((language: string) => {
+        describe(`Language ${language}`, () => {
+            beforeEach(() => {
+                TestBed.configureTestingModule({
+                    providers: [PpwApiTranslatePipe, { provide: LOCALE_ID, useValue: language }]
+                })
+                apiTranslatePipe = TestBed.inject(PpwApiTranslatePipe)
+            })
+
+            it(`should work with a translation map and the given language ${language}`, () => {
+                const result = apiTranslatePipe.transform(mockTranslationMap)
+                expect(result).toEqual(`test${language}`)
+            })
+
+            it('should work with an undefined translation map and the given language', () => {
+                const result = apiTranslatePipe.transform(undefined)
+                expect(result).toEqual('')
+            })
         })
-        apiTranslatePipe = TestBed.inject(PpwApiTranslatePipe)
     })
 
-    it('Should work with PpwTranslationMap input and EN language', () => {
-        const result = apiTranslatePipe.transform(mockTranslationMap)
-        expect(result).toBe(mockTranslationMap['en'])
-    })
+    describe('unknown language in translation map', () => {
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                providers: [PpwApiTranslatePipe, { provide: LOCALE_ID, useValue: 'de' }]
+            })
+            apiTranslatePipe = TestBed.inject(PpwApiTranslatePipe)
+        })
 
-    it('Should work with undefined input and valid language', () => {
-        const result = apiTranslatePipe.transform(undefined)
-        expect(result).toEqual('')
+        it('should return the full object when the language is undefined', () => {
+            const result = apiTranslatePipe.transform(mockTranslationMap)
+            expect(result).toEqual(mockTranslationMap as unknown as string)
+        })
+
+        it('should work with an undefined translation map and the given language', () => {
+            const result = apiTranslatePipe.transform(undefined)
+            expect(result).toEqual('')
+        })
     })
 })
