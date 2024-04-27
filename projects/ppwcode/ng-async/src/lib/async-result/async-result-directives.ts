475669488a08f05ead17857185818f72360ead67
Glen Staes <glen_staes@peopleware.be>
2024-04-27 07:55:51 +0200

    feat(async-result): convert async-result directives to use signal inputs

--- projects/ppwcode/ng-async/src/lib/async-result/async-result-directives.ts
+++ projects/ppwcode/ng-async/src/lib/async-result/async-result-directives.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, Signal } from '@angular/core'
+import { Directive, input, InputSignal, Signal } from '@angular/core'
 import { Observable } from 'rxjs'
 import { AsyncResult } from '../models/async-result'
 import { PpwAsyncResultSuccessContext } from './async-result-success-context'
@@ -8,11 +8,12 @@ import { PpwAsyncResultSuccessContext } from './async-result-success-context'
     selector: '[ppw-async-result-initial]'
 })
 export class PpwAsyncResultInitialDirective<TResult, TFilters> {
-    @Input({ required: true }) public contextTyper!:
+    public contextTyper: InputSignal<
         | Observable<AsyncResult<TResult, TFilters> | null>
         | Signal<AsyncResult<TResult, TFilters> | null>
         | AsyncResult<TResult, TFilters>
         | null
+    > = input.required()
 
     static ngTemplateContextGuard<TResult, TFilters>(
         dir: PpwAsyncResultSuccessDirective<TResult, TFilters>,
@@ -28,11 +29,9 @@ export class PpwAsyncResultInitialDirective<TResult, TFilters> {
     selector: '[ppw-async-result-success]'
 })
 export class PpwAsyncResultSuccessDirective<T, U> {
-    @Input({ required: true }) public contextTyper!:
-        | Observable<AsyncResult<T, U> | null>
-        | Signal<AsyncResult<T, U> | null>
-        | AsyncResult<T, U>
-        | null
+    public contextTyper: InputSignal<
+        Observable<AsyncResult<T, U> | null> | Signal<AsyncResult<T, U> | null> | AsyncResult<T, U> | null
+    > = input.required()
 
     static ngTemplateContextGuard<TResult, TFilters>(
         dir: PpwAsyncResultSuccessDirective<TResult, TFilters>,
--- projects/ppwcode/ng-async/src/lib/async-result/async-result-success-context.ts
+++ projects/ppwcode/ng-async/src/lib/async-result/async-result-success-context.ts
@@ -1,13 +1,14 @@
-import { Signal } from '@angular/core'
+import { InputSignal, Signal } from '@angular/core'
 import { Observable } from 'rxjs'
 import { AsyncResult } from '../models/async-result'
 
 export interface PpwAsyncResultSuccessContext<TResult, TFilters> {
     $implicit: AsyncResult<TResult, TFilters>
-    contextTyper:
+    contextTyper: InputSignal<
         | Observable<AsyncResult<TResult, TFilters> | null>
         | Signal<AsyncResult<TResult, TFilters> | null>
         | AsyncResult<TResult, TFilters>
         | null
+    >
     entity: TResult
 }
