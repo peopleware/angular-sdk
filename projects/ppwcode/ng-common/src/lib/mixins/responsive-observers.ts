00c5b0dc6b72de83ed01acc3e826166a361b6756
Mark Cuypers <mark_cuypers@peopleware.be>
2024-05-07 14:12:32 +0200

    Add mixinResponsiveObservers

---
+++ projects/ppwcode/ng-common/src/lib/mixins/responsive-observers.ts
@@ -0,0 +1,66 @@
+import { inject } from '@angular/core'
+import { map, Observable, shareReplay } from 'rxjs'
+import { Constructor } from './constructor'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+
+/**
+ * Defines helper functions to make responsive designed screen layouts.
+ */
+export interface CanResponsiveObservers {
+    /** Observable to see if screen size is Small. */
+    isSmallScreen$: Observable<boolean>
+    /** Observable to see if screen size is XSmall. */
+    isXSmallScreen$: Observable<boolean>
+    /** Observable to see if screen size is Medium. */
+    isMediumScreen$: Observable<boolean>
+    /** Observable to see if screen size is Large. */
+    isLargeScreen$: Observable<boolean>
+    /** Observable to see if screen size is XLarge. */
+    isXLargeScreen$: Observable<boolean>
+}
+
+/** Constructable type that offers responsive observers helper functions. */
+export type CanResponsiveObserversCtor = Constructor<CanResponsiveObservers>
+
+/**
+ * Mixin to extend the given constructable with functionality to implement responsive design.
+ * Provides the following:
+ * - functions to know the current screensize.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const mixinResponsiveObservers = <T extends Constructor<{}>>(base?: T): CanResponsiveObserversCtor & T => {
+    const baseClass: T = base ?? (class {} as T)
+
+    return class extends baseClass implements CanResponsiveObservers {
+        private breakpointObserver: BreakpointObserver = inject(BreakpointObserver)
+
+        public isXSmallScreen$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.XSmall]).pipe(
+            map((state) => state.matches),
+            shareReplay()
+        )
+        public isSmallScreen$: Observable<boolean> = this.breakpointObserver
+            .observe([Breakpoints.XSmall, Breakpoints.Small])
+            .pipe(
+                map((state) => state.matches),
+                shareReplay()
+            )
+        public isMediumScreen$: Observable<boolean> = this.breakpointObserver
+            .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
+            .pipe(
+                map((state) => state.matches),
+                shareReplay()
+            )
+        public isLargeScreen$: Observable<boolean> = this.breakpointObserver
+            .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large])
+            .pipe(
+                map((state) => state.matches),
+                shareReplay()
+            )
+        public isXLargeScreen$: Observable<boolean> = this.breakpointObserver
+            .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+            .pipe(
+                map((state) => state.matches),
+                shareReplay()
+            )
+    }
+}
