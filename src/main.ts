fd42a6c73352da17cc0b955d448748099f40e139
Mark Cuypers <mark_cuypers@peopleware.be>
2024-05-16 13:52:31 +0200

    Use ppwHttpErrorExtractorWithTranslatedMessages in the example app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { AppModule } from './app/app.module'
+import { ppwHttpErrorExtractorWithTranslatedMessages } from '../projects/ppwcode/ng-async/src/lib/error-extractors'
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch((err) => console.error(err))
+
+window.ppwcodeHttpErrorExtractor = ppwHttpErrorExtractorWithTranslatedMessages
