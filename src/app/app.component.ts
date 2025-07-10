ab1839715978b4dff855ac9cfdab6628ff2e1323
Glen Staes <staeseke@gmail.com>
2025-07-10 22:01:31 +0200

    Refactor example app component to reflect a reactive setup to get the navigation items without triggering a rerendering of the sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject } from '@angular/core'
+import { Component, computed, inject } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
 import { mixinResponsiveObservers } from '@ppwcode/ng-common'
-import { getFullRoutePath } from '@ppwcode/ng-router'
-import { NavigationItem, SidebarOptions } from '@ppwcode/ng-wireframe'
-import { ROUTE_MAP } from './app-routing.module'
+import { SidebarOptions } from '@ppwcode/ng-wireframe'
+import { getNavigationItems } from './app.navigation'
 
 @Component({
     selector: 'ppw-root',
@@ -29,6 +28,17 @@ export class AppComponent extends mixinResponsiveObservers() {
     public toolbarLogoHeight = 40
     public toolbarHeightPx = 60
 
+    public navigationItems = computed(() => {
+        // Even though this a function that is being called, the items array will only change if a reactive property
+        // on the navigation items changes. For example, depending on permissions, you'd typically use an if statement
+        // using a (computed) signal to determine whether a navigation item should be included or not. If that (computed)
+        // signal changes, then this `navigationItems` computed signal will also be updated and getNavigationItems will
+        // be called again.
+
+        // You can pass any dependencies (like services) to this function as a parameter to use them in the function.
+        return getNavigationItems()
+    })
+
     constructor() {
         super()
         this.translate.setDefaultLang('en')
@@ -37,62 +47,5 @@ export class AppComponent extends mixinResponsiveObservers() {
 
     getSidebarOptions(): SidebarOptions {
         return { ...this.sidebarOptions, closedByDefaultOnLargerDevice: this.closedByDefaultOnLargerDevice }
-    }
-
-    public getNavigationItems(): NavigationItem[] {
-        return [
-            {
-                label: this.translate.instant('navigation.dashboard_item'),
-                icon: 'fa-solid fa-grip',
-                fullRouterPath: getFullRoutePath(ROUTE_MAP.dashboardItem)
-            },
-            {
-                label: this.translate.instant('navigation.components'),
-                icon: 'fa-solid fa-cubes',
-                children: [
-                    {
-                        label: this.translate.instant('navigation.confirmation_dialog'),
-                        icon: 'fa-solid fa-circle-question',
-                        fullRouterPath: getFullRoutePath(ROUTE_MAP.confirmationDialog)
-                    },
-                    {
-                        label: this.translate.instant('navigation.expandable_card'),
-                        icon: 'fa-solid fa-house',
-                        fullRouterPath: getFullRoutePath(ROUTE_MAP.expandableCard)
-                    },
-                    {
-                        label: this.translate.instant('navigation.table'),
-                        icon: 'fa-solid fa-laptop-code',
-                        fullRouterPath: getFullRoutePath(ROUTE_MAP.table)
-                    },
-                    {
-                        label: this.translate.instant('navigation.form_table'),
-                        icon: 'fa-solid fa-table-list',
-                        fullRouterPath: getFullRoutePath(ROUTE_MAP.formTable)
-                    },
-                    {
-                        label: this.translate.instant('navigation.message_bar'),
-                        icon: 'fa-solid fa-triangle-exclamation',
-                        fullRouterPath: getFullRoutePath(ROUTE_MAP.messageBar)
-                    }
-                ]
-            },
-            {
-                label: this.translate.instant('navigation.in_memory_logging'),
-                icon: 'fa-solid fa-file-code',
-                fullRouterPath: getFullRoutePath(ROUTE_MAP.inMemoryLogging)
-            },
-            {
-                label: this.translate.instant('navigation.global_error_handler'),
-                icon: 'fa-solid fa-bug',
-                fullRouterPath: getFullRoutePath(ROUTE_MAP.globalErrorHandler)
-            },
-            {
-                label: this.translate.instant('navigation.peopleware_website'),
-                icon: 'fa-solid fa-earth-europe',
-                fullRouterPath: 'https://peopleware.be',
-                isExternalLink: true
-            }
-        ]
     }
 }
