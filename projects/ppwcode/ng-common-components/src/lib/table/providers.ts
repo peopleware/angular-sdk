dc6b83bb91f00e15a9090a19d68bc564e560a5ac
Mark Cuypers <mark_cuypers@peopleware.be>
2025-11-21 16:33:02 +0100

    Typo

--- projects/ppwcode/ng-common-components/src/lib/table/providers.ts
+++ projects/ppwcode/ng-common-components/src/lib/table/providers.ts
@@ -1,6 +1,6 @@
 import { InjectionToken, Type } from '@angular/core'
 
-export const PPW_TABLE_DEFAULT_OPTIONS = new InjectionToken<PpwTableDefaultOptions>('PPWTABLE_DEFAULT_OPTIONS')
+export const PPW_TABLE_DEFAULT_OPTIONS = new InjectionToken<PpwTableDefaultOptions>('PPW_TABLE_DEFAULT_OPTIONS')
 
 export interface PpwTableDefaultOptions {
     dateColumnFormatter?: (value: unknown) => string
