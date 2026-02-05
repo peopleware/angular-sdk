b009c58ff8b4c33e6c45b0d7bea664038cd5a5be
Glen Staes <staeseke@gmail.com>
2026-02-05 19:30:32 +0100

    Add @ppwcode/ng-sdk ng-add schematic that adds the ppwcode Angular SDK dependencies

---
+++ projects/ppwcode/ng-sdk/schematics/ng-add/dependencies/versions.ts
@@ -0,0 +1,10 @@
+export const versions = {
+    crossEnv: '10.1.0',
+    eslintPluginNoSecrets: '2.2.1',
+    fontawesomeFree: '7.1.0',
+    ngxTranslateCore: '17.0.0',
+    ngxTranslateHttpLoader: '17.0.0',
+    prettier: '3.8.1',
+    prettierConfigStandard: '7.0.0',
+    ppwcode: '21.1.1'
+}
