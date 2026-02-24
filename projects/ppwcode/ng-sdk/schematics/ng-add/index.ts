4905f095a649baa33c80720c0aed77df77902ea5
Glen Staes <staeseke@gmail.com>
2026-02-24 20:59:49 +0100

    Extend ng-add schematic to configure Vitest

--- projects/ppwcode/ng-sdk/schematics/ng-add/index.ts
+++ projects/ppwcode/ng-sdk/schematics/ng-add/index.ts
@@ -1,3 +1,4 @@
+import { UnitTestBuilderOptions } from '@angular/build'
 import {
     apply,
     applyTemplates,
@@ -9,11 +10,14 @@ import {
     Tree,
     url
 } from '@angular-devkit/schematics'
+import { workspaces } from '@angular-devkit/core'
+import { TargetDefinition } from '@schematics/angular/utility'
 import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies'
+import { addAngularMaterial } from './angular-material/add'
+import { createHost } from './create-host'
 import { dependencies, devDependencies } from './dependencies/dependencies'
 import { addAngularESLint } from './eslint/add'
 import { modifyESLintConfig } from './eslint/modify'
-import { addAngularMaterial } from './angular-material/add'
 
 export function ngAdd(): Rule {
     return chain([
@@ -21,7 +25,8 @@ export function ngAdd(): Rule {
         addAngularESLint(),
         modifyESLintConfig(),
         copyFilesToRoot(),
-        addAngularMaterial()
+        addAngularMaterial(),
+        addVitestConfig()
     ])
 }
 
@@ -56,4 +61,26 @@ const copyFilesToRoot = () => {
         apply(url('./files'), [applyTemplates({ dot: '.' }), move('/')]),
         MergeStrategy.AllowCreationConflict
     )
+}
+
+const addVitestConfig = () => {
+    return async (host: Tree) => {
+        const workspaceHost = createHost(host)
+        const { workspace } = await workspaces.readWorkspace('/', workspaceHost)
+
+        // For each project in the workspace, update the test target to refer to the vitest config file and tsconfig.spec.json
+        const projectNames = Array.from(workspace.projects.keys())
+        projectNames.forEach((projectName) => {
+            const project = workspace.projects.get(projectName)!
+
+            const testTarget: TargetDefinition = project.targets.get('test') ?? { builder: '@angular/build:unit-test' }
+            const options: UnitTestBuilderOptions = (testTarget.options ??= {})
+            options.runnerConfig = 'vitest.config.ts'
+            options.tsConfig = 'tsconfig.spec.json'
+
+            project.targets.set(projectName, testTarget)
+        })
+
+        await workspaces.writeWorkspace(workspace, workspaceHost)
+    }
 }
