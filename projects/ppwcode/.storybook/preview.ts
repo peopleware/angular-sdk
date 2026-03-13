5ce1e810008debbd535e312f9dfbdb29b5989b63
Mark Cuypers <mark_cuypers@peopleware.be>
2026-03-13 14:25:08 +0100

    Add Storybook to the project

---
+++ projects/ppwcode/.storybook/preview.ts
@@ -0,0 +1,44 @@
+import type { Preview } from '@storybook/angular'
+import { applicationConfig } from '@storybook/angular'
+import { provideZonelessChangeDetection } from '@angular/core'
+import { provideNoopAnimations } from '@angular/platform-browser/animations'
+import { provideTranslateService } from '@ngx-translate/core'
+import { provideRouter } from '@angular/router'
+import { withThemeByClassName } from '@storybook/addon-themes'
+
+const preview: Preview = {
+    decorators: [
+        withThemeByClassName({
+            themes: {
+                light: '',
+                dark: 'dark-theme'
+            },
+            defaultTheme: 'light'
+        }),
+        applicationConfig({
+            providers: [
+                provideZonelessChangeDetection(),
+                provideNoopAnimations(),
+                provideTranslateService(),
+                provideRouter([])
+            ]
+        }),
+        (story, context) => {
+            const link = document.createElement('link')
+            link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
+            link.rel = 'stylesheet'
+            document.head.appendChild(link)
+            return story(context)
+        }
+    ],
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i
+            }
+        }
+    }
+}
+
+export default preview
