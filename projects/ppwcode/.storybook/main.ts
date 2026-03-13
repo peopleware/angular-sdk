7b92ad219b2596c5be17127b156383736992df30
Mark Cuypers <mark_cuypers@peopleware.be>
2026-03-13 14:25:08 +0100

    Add Storybook to the project

---
+++ projects/ppwcode/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/angular'
+
+const config: StorybookConfig = {
+    stories: ['../**/src/**/*.mdx', '../**/src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-themes',
+        '@storybook/addon-a11y',
+        '@storybook/addon-docs',
+        '@storybook/addon-onboarding'
+    ],
+    framework: '@storybook/angular',
+    staticDirs: [
+        {
+            from: '../../../src/assets',
+            to: '/assets'
+        }
+    ]
+}
+export default config
