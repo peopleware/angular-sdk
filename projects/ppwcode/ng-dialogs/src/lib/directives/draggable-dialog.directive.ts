522bb5c38c45d271a9735b8a6e1242a7b89c7d2c
Maarten Somers <maarten_somers@peopleware.be>
2026-07-09 16:14:42 +0200

    Make the cdkDrag rootelementselector for draggable-dialog configurable

--- projects/ppwcode/ng-dialogs/src/lib/directives/draggable-dialog.directive.ts
+++ projects/ppwcode/ng-dialogs/src/lib/directives/draggable-dialog.directive.ts
@@ -1,17 +1,31 @@
 import { CdkDrag } from '@angular/cdk/drag-drop'
-import { Directive, ElementRef, inject, OnInit, Renderer2 } from '@angular/core'
+import { Directive, ElementRef, inject, InjectionToken, input, OnInit, Renderer2, ValueProvider } from '@angular/core'
+
+export const DRAGGABLE_DIALOG_ROOT_ELEMENT_SELECTOR = new InjectionToken<string>(
+    'Draggable dialog root element selector',
+    { factory: () => '.cdk-overlay-pane' }
+)
+
+export const provideDraggableDialogRootElementSelector = (rootElementSelector: string): ValueProvider => ({
+    provide: DRAGGABLE_DIALOG_ROOT_ELEMENT_SELECTOR,
+    useValue: rootElementSelector
+})
 
 @Directive({
     selector: '[ppwDraggableDialog]',
     hostDirectives: [CdkDrag]
 })
 export class DraggableDialogDirective implements OnInit {
+    public readonly rootElementSelector = input(inject(DRAGGABLE_DIALOG_ROOT_ELEMENT_SELECTOR), {
+        alias: 'ppwDraggableDialogRootElementSelector'
+    })
+
     #el: ElementRef = inject(ElementRef)
     #renderer: Renderer2 = inject(Renderer2)
     #cdkDrag: CdkDrag = inject(CdkDrag)
 
     ngOnInit(): void {
         this.#renderer.addClass(this.#el.nativeElement, 'ppw-draggable-dialog')
-        this.#cdkDrag.rootElementSelector = '.cdk-overlay-pane'
+        this.#cdkDrag.rootElementSelector = this.rootElementSelector()
     }
 }
